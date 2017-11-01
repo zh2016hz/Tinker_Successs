2d9362e82b8a11425ab60817e0342d5340fceeca
zhouhao9@xiaoniu66.com <zhouhao9@xiaoniu66.com>
2017-11-01 18:08:54 +0800

    sss

---
+++ app/src/main/java/com/example/xn069392/tencent_tinker_clone/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.xn069392.tencent_tinker_clone;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.shareutil.ShareTinkerInternals;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button loadPatchButton = (Button) findViewById(R.id.loadPatch);
+        loadPatchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),
+                        Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+            }
+        });
+
+        Button killSelfButton = (Button) findViewById(R.id.killSelf);
+
+        killSelfButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareTinkerInternals.killAllOtherProcess(getApplicationContext());
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/xn069392/tencent_tinker_clone/SampleApplicationContext.java
@@ -0,0 +1,9 @@
+package com.example.xn069392.tencent_tinker_clone;
+
+import android.app.Application;
+import android.content.Context;
+
+public class SampleApplicationContext {
+    public static Application application = null;
+    public static Context context = null;
+}
